e0a8613ed29f7138a5e17809fdea2d22a1250a4d
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2022-07-17 15:14:12 +0100

    Added new MLP

--- scripts/MLP_definitions.py
+++ scripts/MLP_definitions.py
@@ -58,4 +58,24 @@ MLP_Settings_Dictionary = {
             5: {"Output": True, "activation":"relu"}
         }
     },
+
+    "4":{
+        
+        "HyperParams": {
+            
+            "learning_rate": 0.0001,
+            "loss_function": "mse",
+            "metrics": "mae"
+        },
+
+        "layers":{
+            1: {"Input":True,"Hidden Nodes": 32, "activation":"relu"},
+            2: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            3: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            4: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            5: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            6: {"Hidden Nodes": 32, "activation":"relu"},
+            7: {"Output": True, "activation":"relu"}
+        }
+    }
 }
