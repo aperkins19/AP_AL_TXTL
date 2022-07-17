e7a342748a5b1e225189ec7797425c9bb5488b04
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2022-07-17 19:51:26 +0100

    Added more MLPs

--- scripts/MLP_definitions.py
+++ scripts/MLP_definitions.py
@@ -77,5 +77,44 @@ MLP_Settings_Dictionary = {
             6: {"Hidden Nodes": 32, "activation":"relu"},
             7: {"Output": True, "activation":"relu"}
         }
+    },
+
+    "5":{
+        
+        "HyperParams": {
+            
+            "learning_rate": 0.000001,
+            "loss_function": "mse",
+            "metrics": "mae"
+        },
+
+        "layers":{
+            1: {"Input":True,"Hidden Nodes": 32, "activation":"relu"},
+            2: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            3: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            4: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            5: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            6: {"Hidden Nodes": 32, "activation":"relu"},
+            7: {"Output": True, "activation":"relu"}
+        }
+    },
+
+    "6":{
+       "HyperParams": {
+            
+            "learning_rate": 0.000001,
+            "loss_function": "mse",
+            "metrics": "mae"
+        },
+
+        "layers":{
+            1: {"Input":True,"Hidden Nodes": 32, "activation":"relu"},
+            2: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            3: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            4: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            5: {"Hidden Nodes": 100, "activation":"relu", "Dropout": 0.2},
+            6: {"Hidden Nodes": 32, "activation":"relu"},
+            7: {"Output": True, "activation":"linear"}
+        }
     }
 }
